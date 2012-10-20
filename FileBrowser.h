823b7effa7615b9fd4a754615363572ce234ca6e
anthony <githuba2g@antcassidy.com>
2012-10-21 01:32:22 +1300

    Fixed licensing

--- FileBrowser.h
+++ FileBrowser.h
@@ -1,7 +1,19 @@
-/*  To copy this you need to follow this license http://creativecommons.org/licenses/by-sa/2.5/
-* Jacek Surazski doesn't endorse this work, but it's a copy of his Qt Quarterly submission.
+/*
+ * Copyright 2012 Anthony Cassidy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 
-*/
 #pragma once
 #include "A2GASSERT.h"
 #include "FOLDERSONLY.h"
